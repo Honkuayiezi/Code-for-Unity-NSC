6b8c42ead30da24aa7a1a5faaab17764ca35ecac
MayReel <69669786+Honkuayiezi@users.noreply.github.com>
2022-12-16 21:58:02 +0700

    Update SpawnObj.cs

--- SpawnObj.cs
+++ SpawnObj.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spawn : MonoBehaviour
+public class SpawnObj : MonoBehaviour
 {
     public GameObject _sphere;
     public GameObject _capsule;
