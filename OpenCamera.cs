3b0dcd9951d350bbf5a103d7cd9bde8e47c00cb4
MayReel <69669786+Honkuayiezi@users.noreply.github.com>
2022-12-16 21:53:06 +0700

    Update OpenCamera.cs

--- OpenCamera.cs
+++ OpenCamera.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PhoneCam : MonoBehaviour
+public class OpenCamera : MonoBehaviour
 {
     private bool camAvailble;
     private WebCamTexture backCam;
